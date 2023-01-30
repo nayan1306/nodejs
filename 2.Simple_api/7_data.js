ebe9a3aa16bf45c4786ef1097f0ebd5f0dd2c157
Nayan Verma <105338541+nayan1306@users.noreply.github.com>
2023-01-30 07:32:06 +0530

    simple api

---
+++ 2.Simple_api/7_data.js
@@ -0,0 +1,5 @@
+const data = [{name:'User1',email:'user1@gmail.com'},
+{name:'User2',email:'user2@gmail.com'},{name:'User3',email:'user3@gmail.com'},{name:'User4',email:'user4@gmail.com'}]
+
+
+module.exports = data;

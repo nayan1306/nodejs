52f00dcf7063962679a78072b80a5e2752cbe8e9
nayan1306 <vnayan1306@gmail.com>
2023-02-11 15:38:26 +0530

    removed extension from url

--- 3.deep_dive/index.js
+++ 3.deep_dive/index.js
@@ -5,6 +5,17 @@ const app = express();
 
 const publicpath = path.join(__dirname,'public');
 
-app.use(express.static(publicpath));
+//app.use(express.static(publicpath));
+app.get('',(_,resp)=>{
+    resp.sendfile(`${publicpath}/index.html`)
+})
+
+app.get('/help',(_,resp)=>{
+    resp.sendfile(`${publicpath}/help.html`)
+})
+
+app.get('/about',(_,resp)=>{
+    resp.sendfile(`${publicpath}/about.html`)
+})
 
 app.listen(4500);

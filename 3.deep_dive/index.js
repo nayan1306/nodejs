02debe9308d00193b4064e79a391cc4cbdf1d1c0
nayan1306 <vnayan1306@gmail.com>
2023-02-17 14:38:56 +0530

    Created dynamic branches

--- 3.deep_dive/index.js
+++ 3.deep_dive/index.js
@@ -6,6 +6,9 @@ const app = express();
 const publicpath = path.join(__dirname,'public');
 
 //app.use(express.static(publicpath));
+
+app.set('view engine','ejs');
+
 app.get('',(_,resp)=>{
     resp.sendfile(`${publicpath}/index.html`)
 })
@@ -16,6 +19,21 @@ app.get('/help',(_,resp)=>{
 
 app.get('/about',(_,resp)=>{
     resp.sendfile(`${publicpath}/about.html`)
+})
+
+app.get('/profile',(_,resp)=>{
+    const user = {
+        name: 'Nayan Verma',
+        email: 'vnayan94@gmail.com',
+        city: 'Raipur',
+        age: '19',
+        skills: ['Flutter','Dart','Html','CSS','JS']   
+    }
+    resp.render('profile',{user})
+})
+
+app.get('*',(_,resp)=>{
+    resp.sendfile(`${publicpath}/errorpage.html`)
 })
 
 app.listen(4500);
